f04e0dc040295771d4279316bf29bb76becccc9a
Fernando Futila <fernando.futila@gmail.com>
2022-09-30 10:33:48 -0300

    Feat: created deleted user function

--- src/components/UsersTable/TableItem/index.tsx
+++ src/components/UsersTable/TableItem/index.tsx
@@ -1,43 +1,45 @@
+import { useState } from "react";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
+import { MdEdit, MdDelete, MdSchedule } from "react-icons/md";
 import { User } from "../../../types/user";
 
-import { MdEdit, MdDelete, MdSchedule } from "react-icons/md";
-import { useState } from "react";
 import { ScheduleModal } from "../../ScheduleModal";
 import { CustomModal } from "../../CustomModal";
+
+import { useUsers } from "../../../hooks/useUsers";
 
 interface TableItemProps {
   user: User;
 }
 
 export function TableItem({ user }: TableItemProps) {
+  const { asyncDeleteUser } = useUsers();
   const [isOpenCustomModal, setIsOpenCustomModal] = useState(false);
   const [isOpenScheduleModal, setIsOpenScheduleModal] = useState(false);
 
-  const toggleModal = (type: string) => {
-    if (type === "custom-modal") {
-      setIsOpenCustomModal(!isOpenCustomModal);
-    }
-
-    if (type === "schedule-modal") {
-      setIsOpenScheduleModal(!isOpenScheduleModal);
-    }
+  const toggleModalCustomModal = () => {
+    setIsOpenCustomModal(!isOpenCustomModal);
   };
+  const toggleModalScheduleModal = () => {
+    setIsOpenScheduleModal(!isOpenScheduleModal);
+  };
+
+  const handleDeleteUser = (userId: number): void => {
+    asyncDeleteUser(user.id);
+  };
+
   return (
     <>
       {!isOpenScheduleModal && isOpenCustomModal && (
         <CustomModal
           user={user}
           type="edit"
-          onRequestClose={() => toggleModal("custom-modal")}
+          onRequestClose={toggleModalCustomModal}
         />
       )}
       {!isOpenCustomModal && isOpenScheduleModal && (
-        <ScheduleModal
-          user={user}
-          onRequestClose={() => toggleModal("custom-modal")}
-        />
+        <ScheduleModal user={user} onRequestClose={toggleModalScheduleModal} />
       )}
       <TableRow>
         <TableCell>{user.id}</TableCell>
@@ -47,13 +49,13 @@ export function TableItem({ user }: TableItemProps) {
         <TableCell>{user.email}</TableCell>
         <TableCell>{user.phone}</TableCell>
         <TableCell className="actions">
-          <button type="button" onClick={() => toggleModal("custom-modal")}>
+          <button type="button" onClick={toggleModalCustomModal}>
             <MdEdit />
           </button>
-          <button type="button">
+          <button type="button" onClick={() => handleDeleteUser(user.id)}>
             <MdDelete />
           </button>
-          <button type="button" onClick={() => toggleModal("schedule-modal")}>
+          <button type="button" onClick={toggleModalScheduleModal}>
             <MdSchedule />
           </button>
         </TableCell>
